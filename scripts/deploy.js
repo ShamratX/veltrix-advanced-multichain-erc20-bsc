c1fb3a1d8e5e61fe573a0f2d8d4480857eaf1ef6
ShamratX <shamrat@MacBook-Pro.local>
2026-06-28 19:43:30 +0600

    added verify command

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -27,10 +27,12 @@ const main = async () => {
         const totalSupplyFormated = ethers.formatUnits(totalSupply ,18);
 
         console.log(`Contract Address: ${contractAddress}`);
-        console.log(`Verify: npx hardhat verify --network ${network.name} ${contractAddress}`);
         console.log(`Deployemnt Tx Hash: ${txHash}`);
         console.log(`Total Supply in Wei: ${totalSupply}`);
-        console.log(`Total Supply Formatted: ${totalSupplyFormated}`);       
+        console.log(`Total Supply Formatted: ${totalSupplyFormated}`);
+        
+        console.log("\nVerify Command:");
+        console.log(`npx hardhat verify --network ${network.name} ${contractAddress}`);    
 
     } 
      catch (error) {
