2fbba0e4e61d548f1eeee2055e4f19049b65a24b
ShamratX <shamrat@MacBook-Pro.local>
2026-06-28 21:12:43 +0600

    Congig

--- hardhat.config.js
+++ hardhat.config.js
@@ -28,7 +28,7 @@ module.exports = {
             chainId: 11155111,
             accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
         },
-        mainnet: {
+        eth: {
             url: ETH_MAINNET_RPC_URL || "",
             chainId: 1,
             accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
